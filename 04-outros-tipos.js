473ff6b96e86ff322e8e4b66f106a237fef13ec7
rony646 <rony646@users.noreply.github.com>
2020-09-15 20:57:56 -0300

    outros tipos

---
+++ 04-outros-tipos.js
@@ -0,0 +1,13 @@
+// Object
+var obj = {
+    firstName: 'Rony',
+    lastName: 'Silva'
+};
+var obj2 = {
+    category: 'category',
+    name: 'name'
+};
+obj2 = 2;
+// Any
+var variable = 4;
+variable = null; // sem error usando any
---
+++ 04-outros-tipos.ts
@@ -0,0 +1,20 @@
+// Object
+
+let obj: object = {
+    firstName: 'Rony',
+    lastName: 'Silva'
+};
+
+let obj2: Object = { // tipo sem validação
+    category: 'category',
+    name: 'name'
+};
+
+obj2 = 2;
+
+
+// Any
+
+let variable: any = 4;
+
+variable = null;// sem error usando any
